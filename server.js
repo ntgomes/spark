68d6489c7087370887909f91451f69042153c756
ntgomes <ntgomes@ncsu.edu>
2022-11-06 14:07:22 -0500

    Remove broadcast from server-side disconnect handling

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId);
+      socket.to(roomId).emit('user-disconnected', userId);
     });
   });
 });
