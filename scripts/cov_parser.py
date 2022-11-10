2169c8e2f4a5f61569a66e0290abbcd30eccbbd5
ntgomes <ntgomes@ncsu.edu>
2022-11-09 19:31:22 -0500

    Add missing space to cov_parser condition

--- scripts/cov_parser.py
+++ scripts/cov_parser.py
@@ -4,7 +4,7 @@ import os
 filename = sys.argv[1]
 with open(filename, "r") as fin:
     for line in fin:
-        if "All files        |" in line:
+        if "All files         |" in line:
             cov = line.split('|')[4].strip()
             print(cov)
             break
