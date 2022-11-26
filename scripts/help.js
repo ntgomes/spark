05edfadefccd879d9162650d65172d6d6d1be513
guptabhishek785 <36363608+guptabhishek785@users.noreply.github.com>
2022-11-26 18:38:52 -0500

    Update and Rename help.py to help.js
    #35

---
+++ scripts/help.js
@@ -0,0 +1,18 @@
+console.log("Welcome to Spark!")
+console.log("The best hands free web conferencing software in Raleigh, NC.")
+console.log()
+
+console.log("-------Help Guide-------")
+console.log()
+console.log("Here are some places where to start:")
+
+console.log(" - First, try to install needed dependencies with \"npm install\".")
+console.log(" - Then, see about running your test cases with \"npm run test\".")
+console.log(" - Now you should be able to launch the app with \"npm run start\" or \"nodemon server.js\".")
+console.log()
+
+console.log("Just visit your localhost:3000 to find the application and start sharing!")
+console.log()
+console.log("--------------")
+console.log()
+console.log("Visit our online documentation (https://github.com/ntgomes/spark/blob/main/README.md) for more support.")
