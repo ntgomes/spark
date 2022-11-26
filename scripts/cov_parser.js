b6cd2af3f13d80f9714a7a92ceeffba703587e83
guptabhishek785 <36363608+guptabhishek785@users.noreply.github.com>
2022-11-26 18:31:40 -0500

    update cov_parser.py to cov_parser.js

---
+++ scripts/cov_parser.js
@@ -0,0 +1,12 @@
+let filename = process.argv[2];
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream(filename)
+  });
+  
+lineReader.on('line', function (line) {
+if (line.includes("All files         |")){ 
+    let cov = (line.split('|')[4]).trim()
+    console.log(cov)
+    return
+    };
+});
